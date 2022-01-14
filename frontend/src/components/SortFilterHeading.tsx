364dbd908f62118e9985938073415013f12482c9
Reis Renneker <reisr3@gmail.com>
2022-01-14 07:52:03 -0500

    feat: added Create button to Contracts list page

--- frontend/src/components/SortFilterHeading.tsx
+++ frontend/src/components/SortFilterHeading.tsx
@@ -1,3 +1,4 @@
+import { PlusIcon } from '@heroicons/react/outline';
 import {
   ChevronDownIcon,
   FilterIcon,
@@ -9,32 +10,47 @@ const SortFilterHeading = () => {
     <div className="pb-5 border-gray-200 sm:flex sm:items-center sm:justify-between">
       <h3 className="text-xl leading-6 font-medium text-gray-900">Contracts</h3>
       <div className="mt-3 sm:mt-0 sm:ml-4">
-        <div className="flex rounded-md shadow-sm">
-          <button
-            type="button"
-            className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-l-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-          >
-            <FilterIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-            <span className="ml-2">Filter</span>
-            <ChevronDownIcon
-              className="ml-2.5 -mr-1.5 h-5 w-5 text-gray-400"
-              aria-hidden="true"
-            />
-          </button>
-          <button
-            type="button"
-            className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-          >
-            <SortAscendingIcon
-              className="h-5 w-5 text-gray-400"
-              aria-hidden="true"
-            />
-            <span className="ml-2">Sort</span>
-            <ChevronDownIcon
-              className="ml-2.5 -mr-1.5 h-5 w-5 text-gray-400"
-              aria-hidden="true"
-            />
-          </button>
+        <div className="flex rounded-md justify-between">
+          <div>
+            <button
+              type="button"
+              className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-l-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+            >
+              <FilterIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+              <span className="ml-2 hidden sm:inline-block">Filter</span>
+              <ChevronDownIcon
+                className="ml-2.5 -mr-1.5 h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </button>
+            <button
+              type="button"
+              className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+            >
+              <SortAscendingIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+              <span className="ml-2 hidden sm:inline-block">Sort</span>
+              <ChevronDownIcon
+                className="ml-2.5 -mr-1.5 h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </button>
+          </div>
+
+          <span className="ml-3">
+            <button
+              type="button"
+              className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+              Create
+            </button>
+          </span>
         </div>
       </div>
     </div>
