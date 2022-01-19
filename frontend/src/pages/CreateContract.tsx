9a56c006513eafe4369b73c6d9cd8b3986b03c08
kuromoka <kuromokapb@gmail.com>
2022-01-20 00:12:57 +0900

    add create-contract component

---
+++ frontend/src/pages/CreateContract.tsx
@@ -0,0 +1,11 @@
+import ContractsContainer from '../components/contracts/ContractsContainer';
+import SortFilterHeading from '../components/SortFilterHeading';
+
+export const CreateContract = () => {
+  return (
+    <>
+      <SortFilterHeading />
+      <ContractsContainer />
+    </>
+  );
+};
