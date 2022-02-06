f6f15b0ef1ac7a9de0583001529348d298bb9a6c
Indeavr <donevskimartin@gmail.com>
2022-02-06 11:50:47 +0200

    (refactor) array of tokens + ownable functions + other improvoments

--- contracts/contracts/TestERC20.sol
+++ contracts/contracts/TestERC20.sol
@@ -1,10 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestERC20 is ERC20 {
-  constructor() ERC20('Test ERC20 Token', 'TestERC20') {
-    _mint(msg.sender, 1000000 * (10**uint256(decimals())));
-  }
+    constructor() ERC20("Test ERC20 Token", "TestERC20") {
+        _mint(msg.sender, 1000000 * (10**uint256(decimals())));
+    }
 }
