6ba9920acac3f58b1b95bb49a9cf6f95905b5163
Devon Weller <dweller@devonweller.com>
2022-02-09 12:33:48 -0600

    Keep semicolon for now to prevent many files from failing checks

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,6 @@ module.exports = {
     $schema: 'http://json.schemastore.org/prettierrc',
     trailingComma: 'es5',
     tabWidth: 2,
-    semi: false,
     singleQuote: true,
     arrowParens: 'always',
     overrides: [
