38931e03936f34359ae7ef10d35397276437fd68
Indeavr <donevskimartin@gmail.com>
2022-02-09 17:34:27 +0200

    (feat): forking test cofing

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -16,56 +16,60 @@ dotenv.config();
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+    const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+    for (const account of accounts) {
+        console.log(account.address);
+    }
 });
 
 task(
-  'copy-compiled',
-  'Adds compiled typechain factories to the frontend code',
-  async (taskArguments, hre) => {
-    try {
-      await fs.copy('./typechain', '../frontend/src/contracts-typechain');
-      console.log('Copy completed!');
-    } catch (err) {
-      console.log('An error occurred while copying the folder.');
-      return console.error(err);
+    'copy-compiled',
+    'Adds compiled typechain factories to the frontend code',
+    async (taskArguments, hre) => {
+        try {
+            await fs.copy('./typechain', '../frontend/src/contracts-typechain');
+            console.log('Copy completed!');
+        } catch (err) {
+            console.log('An error occurred while copying the folder.');
+            return console.error(err);
+        }
     }
-  }
 );
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.9',
-  namedAccounts: {
-    deployer: 0,
-  },
-  networks: {
-    localhost: {
-      live: false,
+    solidity: '0.8.9',
+    namedAccounts: {
+        deployer: 0,
     },
-    hardhat: {
-      chainId: 1337,
-      live: false,
+    networks: {
+        localhost: {
+            live: false,
+        },
+        hardhat: {
+            // forking: {
+            //     url: process.env.RPC_URL || "https://polygon-rpc.com",
+            //     blockNumber: 24238595,
+            // },
+            chainId: 1337,
+            live: false,
+        },
+        ropsten: {
+            url: process.env.ROPSTEN_URL || '',
+            accounts:
+                process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        },
     },
-    ropsten: {
-      url: process.env.ROPSTEN_URL || '',
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    gasReporter: {
+        enabled: process.env.REPORT_GAS !== undefined,
+        currency: 'USD',
     },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: 'USD',
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
 };
 
 export default config;
