f9d5bce3b865bce816a4668779d6e9269dab95a8
Devon Weller <dweller@devonweller.com>
2022-02-14 10:44:09 -0600

    Pulls jobs from the blockchain and displays on the Contracts page.

--- frontend/src/pages/MyContracts.tsx
+++ frontend/src/pages/MyContracts.tsx
@@ -1,12 +1,11 @@
 import ContractsContainer from '../components/contracts/ContractsContainer';
 import SortFilterHeading from '../components/SortFilterHeading';
-import { myContracts } from '../mocks/myContracts';
 
 const MyContracts = () => {
   return (
     <>
       <SortFilterHeading heading="My Contracts" />
-      <ContractsContainer contracts={myContracts} />
+      <ContractsContainer jobs={[]} />
     </>
   );
 };
